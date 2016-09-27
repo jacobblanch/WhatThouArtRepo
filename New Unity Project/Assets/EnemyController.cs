5428df5885027f3acb1f5b7850966517fa28b0d0
Jacob Blanch <1006027@student.sae.edu.au>
2016-09-28 00:08:27 +1000

    Added in all of the basic control and enemy mechanics, Next things to do is 1. Build the level 2. Add in Audio 3. Add in any extra stuff (powerups, special blocks etc)

---
+++ New Unity Project/Assets/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed = 2; 
+
+	public GameObject head;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Movement ();
+
+		if (head == false) {
+			Destroy (this.gameObject);
+		}
+
+	}
+
+	public void Movement () { 
+		transform.Translate (Vector2.left * speed * Time.deltaTime);
+	}
+
+	public void OnCollisionEnter (Collision otherObject) {
+		if (otherObject.gameObject.tag != "Player") {
+			transform.Rotate (Vector2.up * 180);
+		}
+	}
+}
---
+++ New Unity Project/Assets/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	public Rigidbody rb;
+
+	public float jumpPower;
+
+	float distToGround = 0.4f;
+
+	float jumpTime = 0.5f;
+
+	float jumpLimit = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Movement ();
+	
+	}
+
+	public void Movement () {
+		if (Input.GetKey ("d")) {
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+		}
+		else if (Input.GetKey ("a")) {
+			transform.Translate(-Vector2.right * speed * Time.deltaTime);   
+		}
+
+		if (jumpTime > jumpLimit) {
+			if (Input.GetKey ("space")) {
+				if (Physics.Raycast (transform.position, -Vector2.up, distToGround + 0.01f) == true) {
+					rb.AddForce (Vector2.up * jumpPower);
+					jumpTime = 0;
+				} 
+			}
+		} else {
+			jumpTime += Time.deltaTime;
+		}
+	}
+
+	public void OnCollisionEnter (Collision otherObject) {
+		if (otherObject.gameObject.tag == "GoombaStomp") {
+			rb.AddForce (Vector2.up * jumpPower/2);
+			Destroy (otherObject.gameObject);
+		}
+	}
+}
